718666c3e92dc7d1efbdb82b9a0b6431a2f102ef
Manabendu <manabendub@halehitlabs.com>
2025-11-17 17:38:48 +0530

    meta-data extract at this head

--- smiple_media_player/macos/Flutter/GeneratedPluginRegistrant.swift
+++ smiple_media_player/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
+import flutter_media_metadata
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FlutterMediaMetadataPlugin.register(with: registry.registrar(forPlugin: "FlutterMediaMetadataPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
