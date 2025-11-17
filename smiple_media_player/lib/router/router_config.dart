20011ba04000b2f6435fd5e5c1ba37b460c297e6
Manabendu <manabendub@halehitlabs.com>
2025-11-17 18:49:06 +0530

    separate player screen

--- smiple_media_player/lib/router/router_config.dart
+++ smiple_media_player/lib/router/router_config.dart
@@ -1,35 +1,6 @@
-// import 'package:flutter/material.dart';
-// import 'package:go_router/go_router.dart';
-// import 'package:smiple_media_player/home_screen.dart';
-// import 'package:smiple_media_player/player_screen.dart';
-
-// class AppRouter {
-//   final GoRouter appRouter = GoRouter(
-//   routes: <RouteBase>[
-//     GoRoute(
-//       path: '/',
-//       builder: (BuildContext context, GoRouterState state) {
-//         return const HomeScreen();
-//       },
-//       routes: <RouteBase>[
-//         GoRoute(
-//           path: '/player',
-//           builder: (BuildContext context, GoRouterState state) {
-//             return const PlayerScreen();
-//           },
-//         ),
-//       ],
-//     ),
-//   ],
-// );
-// }
-
-
-
 import 'package:go_router/go_router.dart';
 import '../ui/home_screen.dart';
-import '../ui/player_screen.dart';
-
+import '../ui/player/player_screen.dart';
 
 final router = GoRouter(
   routes: [
